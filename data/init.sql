d7f9084596d247d546cf52569e9d1a3b72300388
spyros <spgrammatakis@gmail.com>
2020-06-17 14:10:03 +0300

    installs with 1st post

--- data/init.sql
+++ data/init.sql
@@ -4,8 +4,11 @@ CREATE TABLE posts (
 post_id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 author_id INT(10) NOT NULL,
 title VARCHAR(50) NOT NULL,
-summary VARCHAR(50) NOT NULL,
 body VARCHAR(255) NOT NULL,
 created_at DATE NOT NULL,
 updated_at DATE
 )DEFAULT CHARSET=utf8;
+
+INSERT INTO posts(title, body, author_id, created_at)
+    VALUES("Here's our first post","This is the body of the first post.It is split into paragraphs.",1,now())
+;
