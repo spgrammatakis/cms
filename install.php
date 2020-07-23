6f8dcd83f1b6aa27173ce3abf95f7f56d0db8604
spyros <spgrammatakis@gmail.com>
2020-07-23 12:31:43 +0300

    .

--- install.php
+++ install.php
@@ -35,22 +35,13 @@ function installBlog()
     <head>
         <title>Blog installer</title>
         <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-        <style type="text/css">
-            .box {
-                border: 1px dotted silver;
-                border-radius: 5px;
-                padding: 4px;
-            }
-            .error {
-                background-color: #ff6666;
-            }
-            .success {
-                background-color: #88ff88;
-            }
-        </style>
     </head>
     <body>
-            <div class="success box">
+    <div class="create-admin">
+    <p><a href="register.php">Create Admin Account</a></p>
+    </div>
+    <?php?>
+            <div class="success-box">
                 The database and demo data was created OK.<br>
                 <?php installBlog(); ?>
                 <?php if (installBlog()[0] and installBlog()[1]): ?>
