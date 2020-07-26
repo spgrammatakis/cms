9d96ed50cd9b98ec75dafe33941588691bfee6ef
spyros <spgrammatakis@gmail.com>
2020-07-26 13:21:50 +0300

    wtf

--- lib/common.php
+++ lib/common.php
@@ -94,11 +94,13 @@ public function convertSqlDate($sqlDate)
  */
 public function getPostRow($postId)
 {
-    $sql ="SELECT title, body, created_at FROM posts WHERE post_id = $postId";
+    $sql ="SELECT title, body, created_at FROM posts WHERE post_id = :post_id";
     $this->prepareStmt($sql);
     $this->bind(':post_id',$postId);
     // Let's get a row
-    $this->SingleRow();
+    $this->run();
+    return $this->stmt->fetch();
+    //$this->SingleRow();
 }
 
 /**
