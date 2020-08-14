030fd50781455950e0e828a6090c176a080b1c2d
spyros <spgrammatakis@gmail.com>
2020-08-14 14:18:18 +0300

    .

--- templates/text-editor/text-editor-draft.js
+++ templates/text-editor/text-editor-draft.js
@@ -1,19 +1,5 @@
-function getSelectionText() {
-  var text = "";
-  var activeElement = document.activeElementementement;
-  var activeElementTagName = activeElement ? activeElement.tagName.toLowerCase() : null;
-  if (
-    (activeElementTagName == "textarea") || (activeElementTagName == "input" &&
-    /^(?:text|search|password|tel|url)$/i.test(activeElement.type)) &&
-    (typeof activeElement.selectionStart == "number")
-  ) {
-      text = activeElement.value.slice(activeElement.selectionStart, activeElement.selectionEnd);
-  } else if (window.getSelection) {
-      text = window.getSelection().toString();
-  }
-  return text;
-}
+editor.document.designMode = "On";
 
-document.onmouseup = document.onkeyup = document.onselectionchange = function() {
-document.getElementementById("sel").value = getSelectionText();
-};
+function transform(option, argument) {
+  editor.document.execCommand(option, false, argument);
+}
