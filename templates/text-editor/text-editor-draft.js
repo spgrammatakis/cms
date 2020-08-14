420606beed613950579ef54fb7502fbfbbccaed8
spyros <spgrammatakis@gmail.com>
2020-08-14 13:56:00 +0300

    /

--- templates/text-editor/text-editor-draft.js
+++ templates/text-editor/text-editor-draft.js
@@ -1,5 +1,19 @@
-editor.document.designMode = "On";
-
-function transform(option, argument) {
-  editor.document.execCommand(option, false, argument);
+function getSelectionText() {
+  var text = "";
+  var activeEl = document.activeElement;
+  var activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
+  if (
+    (activeElTagName == "textarea") || (activeElTagName == "input" &&
+    /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) &&
+    (typeof activeEl.selectionStart == "number")
+  ) {
+      text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
+  } else if (window.getSelection) {
+      text = window.getSelection().toString();
+  }
+  return text;
 }
+
+document.onmouseup = document.onkeyup = document.onselectionchange = function() {
+document.getElementById("sel").value = getSelectionText();
+};
