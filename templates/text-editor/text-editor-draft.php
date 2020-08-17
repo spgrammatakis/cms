fc88dd6a349e9bb9edb2d7ea14f0d1f9be2eedcc
spyros <spgrammatakis@gmail.com>
2020-08-17 17:50:21 +0300

    progress on text-editor

--- templates/text-editor/text-editor-draft.php
+++ templates/text-editor/text-editor-draft.php
@@ -1,18 +1,5 @@
 <html>
 <link rel="stylesheet" type="text/css" href="text-editor-draft.css">
-Selection:
-<br>
-<textarea id="sel" rows="3" cols="50"></textarea>
-<p>Please select some text.</p>
-<input value="Some text in a text input">
-<br>
-<input type="search" value="Some text in a search input">
-<br>
-<input type="tel" value="4872349749823">
-<br>
-<textarea>Some text in a textarea</textarea>
-<script src="text-editor-draft.js"></script>
-
 <hr style='border: 5px solid red;'>
 <h3>Edit your post</h3>
 <form method="post">
@@ -29,13 +16,24 @@ Selection:
         <label for="post-body">
             Website:
         </label>
-        <textarea
-            id="post-body"
+
+        <input
             name="post-body"
-            rows="8"
-            cols="70"
-        >Lorem ipsum Body</textarea>
+            style="display:none"
+        >
+</br>
+<ul>
+  <li><input type="button" class="button" value="Bold"></li>
+  <li><input type="button" class="button" value="Italic"></li>
+  <li><input type="button" class="button" value="Underline"></li>
+  <li><input type="button" class="button" value="Copy"></li>
+  <li><input type="button" class="button" value="Paste"></li>
+</ul>
+        <div id="post-body-editor" contenteditable=true>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis aliquam nisi laborum quos esse ex saepe? Enim odio deleniti id velit neque praesentium esse. Optio quia exercitationem illum ipsum doloremque!
+        </div>
     </p>
     <input type="submit" value="Finish Edit" />
 </form>
+<script src="text-editor-draft.js"></script>
 </html>
