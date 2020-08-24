87a7635f7366b0efd3bb1d141da92bbfafce7c93
spyros <spgrammatakis@gmail.com>
2020-08-24 17:14:19 +0300

    .

--- templates/post-edit-form.php
+++ templates/post-edit-form.php
@@ -1,4 +1,7 @@
 <html>
+<head>
+<link rel="stylesheet" type="text/css" href="/template/text-editor/text-editor.css">
+</head>
 <body>
 <form method="post" id="post-edit" onsubmit="copyConent()" >
 <h3>Edit your post</h3>
@@ -19,7 +22,7 @@
             class="post-body-textarea"
         ></textarea>
 </br>
-<iframe id="post-editor-iframe" width="50%" height="600" frameBorder="0" onload="iframeInit()" src="../templates/text-editor/text-editor.html">
+<iframe id="post-editor-iframe" width="50%" height="600" onload="iframeInit()" style="border-style:none" src="../templates/text-editor/text-editor.html">
 </iframe>
     </p>
     <input type="submit" value="Finish Edit"/>
