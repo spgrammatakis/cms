301cbb763473fd0925ef6133c430d0963deaf4e9
spyros <spgrammatakis@gmail.com>
2020-10-09 11:29:30 +0300

    .

--- lib/SessionManager.php
+++ lib/SessionManager.php
@@ -16,6 +16,7 @@ class SessionManager extends DbConnection{
     public function sessionCheck(){
         print_r($_COOKIE);
         if($this->getUserName() === "guest"){
+            $this->setCookiesParams();
             echo "Guest Account";
             return;
         }
@@ -63,7 +64,7 @@ class SessionManager extends DbConnection{
     }
 
     private function setCookiesParams(){
-        if(!isset($_COOKIE['user_name']) || empty($_COOKIE['user_name'])){
+        if(!isset($_COOKIE['user_name']) || empty($_COOKIE['user_name'])|| $this->getUserName === "guest"){
             setcookie("user_name", "guest", [
                 "expires" => mktime(0, 0, 0, date("m"),   date("d"),   date("Y")+1),
                 "path" => '/',
@@ -72,7 +73,7 @@ class SessionManager extends DbConnection{
                 "httponly" => true,
                 "samesite" => "Strict"]);
             }
-        if(!isset($_COOKIE['session_token']) || empty($_COOKIE['session_token'])){    
+        if(!isset($_COOKIE['session_token']) || empty($_COOKIE['session_token']) || $this->getUserName === "guest"){    
             setcookie("session_token", bin2hex(random_bytes(20)), [
             "expires" => mktime(0, 0, 0, date("m"),   date("d"),   date("Y")+1),
             "path" => '/',
