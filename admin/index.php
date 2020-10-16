57072e61270daef2f151a8e455dc3a0df19020c6
spyros <spgrammatakis@gmail.com>
2020-10-16 14:38:38 +0300

    .

--- admin/index.php
+++ admin/index.php
@@ -1,13 +1,5 @@
 <?php
 require dirname(__DIR__, 1) . '/vendor/autoload.php';
-// spl_autoload_register('autoload');
-
-
-// function autoload($className){
-//     $filename = dirname(__DIR__, 1).'/'.$className.'.php';
-//     require_once str_replace('\\','/',$filename);
-// }
-//if(!isset($_COOKIE["user_name"]) || empty($_COOKIE['user_name'])) echo "axne";
 $username = $_COOKIE['user_name'] ?? "guest";
 $session = new lib\SessionManager($username);
 $session->sessionCheck();
