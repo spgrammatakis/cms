aa7a46177cd20b0c3953a1c224599f5dd091bad5
spyros <spgrammatakis@gmail.com>
2020-10-23 16:56:40 +0300

    progress on admin user dashboard

--- admin/admin.js
+++ admin/admin.js
@@ -4,10 +4,17 @@ function redirectToEditUser(id) {
 
     var button = document.getElementsByTagName('button');
         for(let i = 0;i<button.length;i++){
-            if(button[i].className === "user-table-edit-button"){
+            if(button[i].className === "user-table-edit-button"){                
                 button[i].addEventListener("click", function(){
                     let  res = button[i].parentElement.childNodes[1].innerText.split("\t");
                     redirectToEditUser(res[1]);
+                    var vis = button[i].parentElement.parentElement.childNodes[3];
+                    console.log(vis);
+                    if (vis.style.visibility === "hidden") {
+                        vis.style.visibility = "visible";
+                      } else {
+                        vis.style.visibility = "hidden";
+                      }
                 });
             }
         }
