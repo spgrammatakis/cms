983d89c3128994a8f29510a78c9a04fde51fc342
spyros <spgrammatakis@gmail.com>
2020-10-25 00:42:05 +0300

    .

--- admin/admin.js
+++ admin/admin.js
@@ -10,11 +10,7 @@ function redirectToEditUser(id) {
                 button[i].addEventListener("click", function(){
                     let  res = button[i].parentElement.childNodes[1].innerText.split("\t");
                     redirectToEditUser(res[1]);           
-                    if (button[i].parentElement.parentElement.childNodes[3].style.visibility === "hidden") {
-                        button[i].parentElement.parentElement.childNodes[3].style.visibility = "visible";
-                      } else {
-                        button[i].parentElement.parentElement.childNodes[3].style.visibility = "hidden";
-                      }
+                    
                 });
             }
         }
