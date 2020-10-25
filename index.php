c982c320e9118cd4444f767a2170f9020fbd1838
spyros <spgrammatakis@gmail.com>
2020-10-25 17:47:51 +0200

    .

--- index.php
+++ index.php
@@ -17,32 +17,32 @@ $session->sessionCheck();
         require 'templates/title.php';
         $postHandler = new lib\PostManager();
         $row = $postHandler->getPosts();
-        for($i = 0, $size = count($row); $i < $size; ++$i):
+        for($p = 0; $p  < count($row); ++$p):
         ?>
 <div class="container">
     <article>
         <header>
-            <h1 class="post-title"><?php echo lib\Utilities::htmlEscape($row[$i]['title']);?></h1>
-            <p class="post-body"><?php echo lib\Utilities::htmlEscape($row[$i]['body']); ?></p>
-            <p><time class="post-date"><?php echo lib\Utilities::convertSqlDate($row[$i]['created_at']); ?></time><p>
+            <h1 class="post-title"><?php echo lib\Utilities::htmlEscape($row[$p]['title']);?></h1>
+            <p class="post-body"><?php echo lib\Utilities::htmlEscape($row[$p]['body']); ?></p>
+            <p><time class="post-date"><?php echo lib\Utilities::convertSqlDate($row[$p]['created_at']); ?></time><p>
         </header>
         <section>
             <h1 class="comments">Comments</h1>
             <footer>
                 <?php
-                $comment = $postHandler->getCommentsForPost($row[$i]['post_id']);
-                for($c = 0, $size = count($comment); $c < $size; ++$c):
+                $comment = $postHandler->getCommentsForPost($row[$p]['post_id']);
+                for($c = 0; $c < count($comment); ++$c):
                 ?>
                 <p>Posted by: <span><?php echo lib\Utilities::htmlEscape($comment[$c]['user_name']); ?></span></p>
                 <p><?php echo lib\Utilities::htmlEscape($comment[$c]['content']); ?></p>
                 <p><time><?php echo lib\Utilities::htmlEscape($comment[$c]['created_at']);?></time></p>
                 <p><?php echo lib\Utilities::htmlEscape($comment[$c]['website']); ?></p>
-                <p><?php echo $postHandler->countCommentsForPost($row[$i]['post_id']). " comments"; ?></p>
+                <p><?php echo $postHandler->countCommentsForPost($row[$p]['post_id']). " comments"; ?></p>
                 <?php endfor; ?>
             </footer>
         </section>
         <section>
-        <p><?php echo "<a href='/view-post.php?post_id=". lib\Utilities::htmlEscape($row[$i]['post_id']) ."'>Read more...</a>";?></p>
+        <p><?php echo "<a href='/view-post.php?post_id=". lib\Utilities::htmlEscape($row[$p]['post_id']) ."'>Read more...</a>";?></p>
         </section>
     </article>
 </div>
