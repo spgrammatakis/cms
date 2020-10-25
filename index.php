11f8b4e9c9c8d8a12fea3818e23444e8c1b982c9
spyros <spgrammatakis@gmail.com>
2020-10-25 17:45:50 +0200

    .

--- index.php
+++ index.php
@@ -16,9 +16,37 @@ $session->sessionCheck();
         <?php
         require 'templates/title.php';
         $postHandler = new lib\PostManager();
-        $postHandler->getPosts();
+        $row = $postHandler->getPosts();
+        for($i = 0, $size = count($row); $i < $size; ++$i):
         ?>
-        <a href="./setup.php">Setup</a>
-        
+<div class="container">
+    <article>
+        <header>
+            <h1 class="post-title"><?php echo lib\Utilities::htmlEscape($row[$i]['title']);?></h1>
+            <p class="post-body"><?php echo lib\Utilities::htmlEscape($row[$i]['body']); ?></p>
+            <p><time class="post-date"><?php echo lib\Utilities::convertSqlDate($row[$i]['created_at']); ?></time><p>
+        </header>
+        <section>
+            <h1 class="comments">Comments</h1>
+            <footer>
+                <?php
+                $comment = $postHandler->getCommentsForPost($row[$i]['post_id']);
+                for($c = 0, $size = count($comment); $c < $size; ++$c):
+                ?>
+                <p>Posted by: <span><?php echo lib\Utilities::htmlEscape($comment[$c]['user_name']); ?></span></p>
+                <p><?php echo lib\Utilities::htmlEscape($comment[$c]['content']); ?></p>
+                <p><time><?php echo lib\Utilities::htmlEscape($comment[$c]['created_at']);?></time></p>
+                <p><?php echo lib\Utilities::htmlEscape($comment[$c]['website']); ?></p>
+                <p><?php echo $postHandler->countCommentsForPost($row[$i]['post_id']). " comments"; ?></p>
+                <?php endfor; ?>
+            </footer>
+        </section>
+        <section>
+        <p><?php echo "<a href='/view-post.php?post_id=". lib\Utilities::htmlEscape($row[$i]['post_id']) ."'>Read more...</a>";?></p>
+        </section>
+    </article>
+</div>
+        <?php endfor; ?>
+        <a href="./setup.php">Setup</a>        
     </body>
 </html>
