347f5cb3de5115184fe9a723eb20dd06c9433c30
spyros <spgrammatakis@gmail.com>
2020-11-10 17:11:48 +0200

    fixes

--- lib/users/edit-user.php
+++ lib/users/edit-user.php
@@ -27,10 +27,9 @@ if( (($session->getUserName() !== $userRow['username']) && ($userPrivileges['edi
 }
 if($_POST){
     $xsrfToken = hash_hmac('sha256', basename($_SERVER['PHP_SELF']), $userHandler->getUserIDFromName($username));
-    $xsrf_err = "Invalid Token";
-    echo $xsrfToken."</br>";
-    echo $_POST['xsrf'];
     if (!(hash_equals($xsrfToken, $_POST['xsrf']))) {
+        $xsrf_err = "Invalid Token";
+        exit;
         }
     $userData = array(
         "user-id" => $userRow['user_id'],
